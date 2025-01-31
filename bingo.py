a2d0898661ab9dc7d41961d5a0e175c59c48781f
Davi <davi.miranda@escolar.ifrn.edu.br>
2025-01-31 10:32:31 -0300

    bingo versão problemática

--- bingo.py
+++ bingo.py
@@ -21,6 +21,9 @@ def criar_cartela(linhas, colunas):
       item += 1
   return cartela
 
+
+sorteio = set()
+
 print("Bem vindo ao nosso bingo :)\n")
 print("Qual modo você gostaria de jogar?")
 print("Nível acelerado: digite 1")
@@ -46,6 +49,11 @@ if nivel==1:
 
   print("Jogador 1:")
   jog1 = criar_cartela(linhas,colunas)
+  jog1_lista = []
+  for i in range(linhas):
+     for j in range(colunas):
+        jog1_lista.append(jog1[i][j])
+
   for i in range(linhas):
     print('|', end=" ")
     for j in range(colunas):
@@ -55,11 +63,33 @@ if nivel==1:
 
   print("Jogador 2:")
   jog2= criar_cartela(linhas,colunas)
+
+  jog2_lista = []
+  for i in range(linhas):
+     for j in range(colunas):
+        jog2_lista.append(jog2[i][j])
+
   for i in range(linhas):
     print('|', end=" ")
     for j in range(colunas):
         print(jog2[i][j], end=" ")
     print('|')
+    while True:
+       num = random.randint(1, 40)
+       sorteio.add(num)
+       print(f"A dezena sorteada foi: {num}")
+       for i in range(linhas):
+          for j in range(colunas):
+             if 
+
+
+#   while  jog1_lista not in sorteio or jog2_lista not in sorteio:
+#     num = random.randint(1,40)
+#     sorteio.append(num)
+#     print(f"A dezena sorteada foi: {num}")
+#     print(f"Números já sorteados: {sorteio}")
+#     print("Hello world")
+#     a = input()
 
 if nivel==2:
   linhas = 3
